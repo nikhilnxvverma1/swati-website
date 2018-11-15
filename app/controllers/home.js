595154450f5cc734eeab5071d263cd03416868b3
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-11-15 14:19:48 +0530

    Migrated from static to a dyanmic server application using handlebars

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -9,8 +9,34 @@ module.exports = (app) => {
 router.get('/', (req, res, next) => {
   db.Article.findAll().then((articles) => {
     res.render('index', {
-      title: 'Generator-Express MVC',
-      articles: articles
+		activeLink:getActiveLinkOn('work'),
+		layout:"header-body-footer"
     });
   });
 });
+
+router.get('/about', (req, res, next) => {
+	db.Article.findAll().then((articles) => {
+	  res.render('about', {
+		activeLink:getActiveLinkOn('about'),
+		layout:"header-body-footer"
+	  });
+	});
+});
+
+function getActiveLinkOn(link){
+	const activeLink={
+		work:false,
+		about:false,
+		blog:false,
+	}
+	switch(link){
+		case "work":activeLink.work=true;
+		break;
+		case "about":activeLink.about=true;
+		break;
+		case "blog":activeLink.blog=true;
+		break;
+	}
+	return activeLink;
+}
